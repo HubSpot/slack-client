c1f4eb7f6b636a2d14022e941250c84a4bae650a
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-04-02 14:14:22 -0400

    parse team id from slack event

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/SlackEvent.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/SlackEvent.java
@@ -1,12 +1,18 @@
 package com.hubspot.slack.client.models.events;
 
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.slack.client.models.events.json.EventDeserializer;
 
+@JsonNaming(SnakeCaseStrategy.class)
 @JsonDeserialize(using = EventDeserializer.class)
 public interface SlackEvent {
   SlackEventType getType();
   String getTs();
+  Optional<String> getTeamId();
 
   default <T extends SlackEvent> T toDetailedEvent() {
     return (T) this;
