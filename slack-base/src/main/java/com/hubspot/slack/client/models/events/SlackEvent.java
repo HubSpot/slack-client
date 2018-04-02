21287563d1deb3238dfdc7ac8b3ecb763d60f68e
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-04-02 17:11:07 -0400

    remove unnecessary annotation

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/SlackEvent.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/SlackEvent.java
@@ -1,11 +1,8 @@
 package com.hubspot.slack.client.models.events;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.slack.client.models.events.json.EventDeserializer;
 
-@JsonNaming(SnakeCaseStrategy.class)
 @JsonDeserialize(using = EventDeserializer.class)
 public interface SlackEvent {
   SlackEventType getType();
