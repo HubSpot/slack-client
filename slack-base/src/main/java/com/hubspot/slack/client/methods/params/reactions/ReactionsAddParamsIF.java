d469e06168a67722bdad54b3896688b05407a323
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-04-03 13:29:19 -0400

    validate for required fields

--- slack-base/src/main/java/com/hubspot/slack/client/methods/params/reactions/ReactionsAddParamsIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/reactions/ReactionsAddParamsIF.java
@@ -2,22 +2,32 @@ package com.hubspot.slack.client.methods.params.reactions;
 
 import java.util.Optional;
 
+import org.immutables.value.Value;
 import org.immutables.value.Value.Immutable;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.immutables.validation.ImmutableConditions;
 
 @Immutable
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
 public interface ReactionsAddParamsIF {
- String getName();
- /**
-  * One of file, file_comment, or the combination of channel and timestamp must be specified.
-  **/
- Optional<String> getChannel();
- Optional<String> getFile();
- Optional<String> getFileComment();
- Optional<String> getTimestamp();
+  String getName();
+  /**
+   * One of file, file_comment, or the combination of channel and timestamp must be specified.
+   **/
+  Optional<String> getChannel();
+  Optional<String> getTimestamp();
+  Optional<String> getFile();
+  Optional<String> getFileComment();
+
+  @Value.Check
+  default void reactionRecipientExists() {
+    String err = "One of file, fileComment, or the combination of channel and timestamp must be specified";
+    ImmutableConditions.checkValid((getChannel().isPresent() && getTimestamp().isPresent())
+        || getFile().isPresent()
+        || getFileComment().isPresent(), err);
+  }
 }
