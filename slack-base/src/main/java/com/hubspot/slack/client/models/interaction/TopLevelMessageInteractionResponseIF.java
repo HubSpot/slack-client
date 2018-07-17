7df7f4ad6a53ec9b5fe0d8f5c0ddd074a7564d78
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-07-17 16:18:32 -0400

    add support for top level message responses

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/TopLevelMessageInteractionResponseIF.java
@@ -0,0 +1,25 @@
+package com.hubspot.slack.client.models.interaction;
+
+import org.immutables.value.Value.Default;
+import org.immutables.value.Value.Immutable;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.TopLevelMessageResponseType;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+public interface TopLevelMessageInteractionResponseIF {
+  TopLevelMessageResponseType getResponseType();
+
+  boolean getReplaceOriginal();
+
+  @Default
+  default boolean getDeleteOriginal() {
+    return false;
+  }
+
+  String getText();
+}
