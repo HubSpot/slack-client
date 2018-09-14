38c6f49968855682bc8cbc989d44bc7ce5416095
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-09-14 14:16:05 -0400

    dont make auth revoke a parameter

--- slack-base/src/main/java/com/hubspot/slack/client/methods/params/auth/AuthRevokeParamsIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/auth/AuthRevokeParamsIF.java
@@ -1,14 +1,12 @@
 package com.hubspot.slack.client.methods.params.auth;
 
 import org.immutables.value.Value.Immutable;
-import org.immutables.value.Value.Parameter;
 
 import com.hubspot.immutables.style.HubSpotStyle;
 
 @Immutable
 @HubSpotStyle
 public interface AuthRevokeParamsIF {
-  @Parameter
   default boolean isTest() {
     return false;
   }
