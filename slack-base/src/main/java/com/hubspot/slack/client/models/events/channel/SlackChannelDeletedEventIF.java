707a462147d6590229c2648c74f9b67f26b2f29f
Bryan Wehner <bwehner@hubspot.com>
2018-09-17 13:43:45 -0400

    Added slack channel events

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/channel/SlackChannelDeletedEventIF.java
@@ -0,0 +1,19 @@
+package com.hubspot.slack.client.models.events.channel;
+
+import org.immutables.value.Value.Immutable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.events.SlackEvent;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+@JsonDeserialize(as = SlackChannelDeletedEvent.class)
+public interface SlackChannelDeletedEventIF extends SlackEvent {
+  @JsonProperty("channel")
+  String getChannelId();
+}
---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/channel/SlackChannelUnarchiveEventIF.java
@@ -0,0 +1,22 @@
+package com.hubspot.slack.client.models.events.channel;
+
+import org.immutables.value.Value.Immutable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.events.SlackEvent;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+@JsonDeserialize(as = SlackChannelUnarchiveEvent.class)
+public interface SlackChannelUnarchiveEventIF extends SlackEvent {
+  @JsonProperty("channel")
+  String getChannelId();
+
+  @JsonProperty("user")
+  String getUnarchivedByUserId();
+}
