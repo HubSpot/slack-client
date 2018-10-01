d5529356414ab0c323b1323c60031860c3e401fb
Bryan Wehner <bwehner@hubspot.com>
2018-10-01 12:29:17 -0400

    Clean up

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/SlackEventView.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/SlackEventView.java
@@ -1,7 +1,5 @@
 package com.hubspot.slack.client.models.events;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
 public interface SlackEventView {
-  public static interface Internal {}
+  interface Internal {}
 }
