3b7c700f55a651d107ea57b3b7f4b70c95334c55
Bryan Wehner <bwehner@hubspot.com>
2018-11-06 12:37:09 -0500

    removed test to put in different PR and fixed ByMethodRateLimiter

--- slack-java-client/src/main/java/com/hubspot/slack/client/ratelimiting/ByMethodRateLimiter.java
+++ slack-java-client/src/main/java/com/hubspot/slack/client/ratelimiting/ByMethodRateLimiter.java
@@ -4,10 +4,16 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import com.google.common.util.concurrent.RateLimiter;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import com.hubspot.slack.client.methods.SlackMethod;
 
+@Singleton
 public class ByMethodRateLimiter implements SlackRateLimiter {
   private static final ConcurrentMap<SlackMethod, RateLimiter> RATE_LIMITERS = new ConcurrentHashMap<>();
+
+  @Inject
+  public ByMethodRateLimiter() {}
 
   @Override
   public double acquire(String slackToken, SlackMethod slackMethod) {
