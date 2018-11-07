2bd9cd4efe824302136581ee962a14b91d4be538
Bryan Wehner <bwehner@hubspot.com>
2018-11-07 11:53:20 -0500

    Moved test module into static class

--- slack-java-client/src/test/java/com/hubspot/slack/client/SlackClientModuleTest.java
+++ slack-java-client/src/test/java/com/hubspot/slack/client/SlackClientModuleTest.java
@@ -2,6 +2,7 @@ package com.hubspot.slack.client;
 
 import org.junit.Test;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Stage;
 
@@ -9,6 +10,17 @@ public class SlackClientModuleTest {
 
   @Test
   public void itGuices() {
-    Guice.createInjector(Stage.TOOL, new SlackClientTestModule());
+    Guice.createInjector(Stage.TOOL, new StrictGuiceModule());
+  }
+
+  private static class StrictGuiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+      binder().requireAtInjectOnConstructors();
+      binder().requireExactBindingAnnotations();
+
+      install(new SlackClientModule());
+    }
   }
 }
