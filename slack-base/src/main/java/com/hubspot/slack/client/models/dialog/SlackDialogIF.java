d9fd653cc34bb5fc1bccb591a6379eac2b8536d7
mlr46 <mlr46@users.noreply.github.com>
2019-01-30 13:01:51 -0500

    Same line for ifPresent check

--- slack-base/src/main/java/com/hubspot/slack/client/models/dialog/SlackDialogIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/dialog/SlackDialogIF.java
@@ -52,18 +52,16 @@ public interface SlackDialogIF {
       throw new IllegalStateException("At most 5 form elements allowed, got " + getElements().size());
     }
 
-    getState()
-        .ifPresent(state -> {
-          if (state.length() > 3000) {
-            throw new IllegalStateException("State cannot exist 3000 chars, got " + state.length());
-          }
-        });
+    getState().ifPresent(state -> {
+      if (state.length() > 3000) {
+        throw new IllegalStateException("State cannot exist 3000 chars, got " + state.length());
+      }
+    });
 
-    getSubmitLabel()
-        .ifPresent(submitLabel -> {
-          if (submitLabel.length() > 24) {
-            throw new IllegalStateException("Submit label cannot exceed 24 chars, got " + submitLabel.length());
-          }
-        });
+    getSubmitLabel().ifPresent(submitLabel -> {
+      if (submitLabel.length() > 24) {
+        throw new IllegalStateException("Submit label cannot exceed 24 chars, got " + submitLabel.length());
+      }
+    });
   }
 }
