23bee7abcad097fd88ed03c26b261cfbb073eb35
kmclarnon <kmclarnon@hubspot.com>
2019-05-30 10:11:57 -0400

    add guice back

---
+++ slack-java-client/src/main/java/com/hubspot/slack/client/SlackClientModule.java
@@ -0,0 +1,28 @@
+package com.hubspot.slack.client;
+
+import com.google.inject.AbstractModule;
+import com.hubspot.slack.client.http.NioHttpClient;
+import com.hubspot.slack.client.http.NioHttpClientFactory;
+
+/**
+ * Use slack-java-client-guice module instead
+ */
+@Deprecated
+public class SlackClientModule extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    bind(NioHttpClient.Factory.class).toInstance(NioHttpClientFactory.defaultFactory());
+    bind(SlackWebClient.Factory.class).toInstance(SlackClientFactory.defaultFactory());
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return o != null && getClass().equals(o.getClass());
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+}
