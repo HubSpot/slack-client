3069044f2f05cf3687dececa78084c4c088f0ded
kmclarnon <kmclarnon@hubspot.com>
2019-07-08 14:25:40 -0400

    add duplicate binding test

--- slack-java-client-guice/src/test/java/com/hubspot/slack/client/guice/SlackClientModuleTest.java
+++ slack-java-client-guice/src/test/java/com/hubspot/slack/client/guice/SlackClientModuleTest.java
@@ -4,13 +4,14 @@ import org.junit.Test;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
-import com.google.inject.Stage;
+import com.hubspot.slack.client.SlackClientFactory;
 
 public class SlackClientModuleTest {
 
   @Test
   public void itGuices() {
-    Guice.createInjector(Stage.TOOL, new StrictGuiceModule());
+    Guice.createInjector(new StrictGuiceModule())
+        .getInstance(SlackClientFactory.class);
   }
 
   private static class StrictGuiceModule extends AbstractModule {
@@ -21,5 +22,18 @@ public class SlackClientModuleTest {
 
       install(new SlackClientModule());
     }
+  }
+
+  @Test
+  public void itCanHaveBothModulesInstalled() {
+    Guice.createInjector(new AbstractModule() {
+      @Override
+      protected void configure() {
+        binder().requireAtInjectOnConstructors();
+
+        install(new SlackClientModule());
+        install(new com.hubspot.slack.client.SlackClientModule());
+      }
+    }).getInstance(SlackClientFactory.class);
   }
 }
