f2fb4485bca647e9b8769ec4f4c83f389111201d
cconnors <cconnors@hubspot.com>
2019-09-11 16:44:34 -0700

    Adding back SlackReplyMessage since Slack is deprecating any reply object.

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/util/AbstractSlackReplyMessage.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/util/AbstractSlackReplyMessage.java
@@ -1,9 +1,13 @@
 package com.hubspot.slack.client.models.events.util;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.immutables.value.Value.Default;
 import org.immutables.value.Value.Immutable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
@@ -16,6 +20,9 @@ import com.hubspot.slack.client.models.events.SlackEventMessageBase;
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
 public abstract class AbstractSlackReplyMessage extends SlackEventMessageBase implements HasUser {
+
+  Logger LOG = LoggerFactory.getLogger(SlackReplyMessage.class);
+
   public abstract String getThreadTs();
 
   @Override
@@ -26,10 +33,28 @@ public abstract class AbstractSlackReplyMessage extends SlackEventMessageBase im
   @JsonProperty("channel")
   public abstract String getChannelId();
 
-  public abstract List<Reply> getReplies();
-
   @Default
   public int getReplyCount() {
     return 0;
+  }
+
+  @JsonProperty("reply_users")
+  public abstract Optional<List<String>> getReplyUserIds();
+
+  public abstract Optional<Integer> getReplyUsersCount();
+
+  @JsonProperty("latest_reply")
+  public abstract Optional<String> getLatestReplyTimestamp();
+
+  /**
+   * @deprecated use {@link #getReplyUserIds()} or {@link #getLatestReplyTimestamp()}
+   * These can be used to find the user ids and the last reply timestamp.
+   * Used to return a list of `Reply`
+   */
+  @Deprecated
+  @Default
+  public List<Reply> getReplies() {
+    LOG.error("Method getReplies() is now deprecated. Slack will stop supporting completely on Oct 18th 2019");
+    return Collections.emptyList();
   }
 }
