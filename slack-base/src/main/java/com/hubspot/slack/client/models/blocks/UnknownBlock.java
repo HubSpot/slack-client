a2e68a03b19a71e8088b097a9c56e11632ddf6e8
John Farrelly <jfarrelly@hubspot.com>
2019-12-17 12:01:32 +0000

    Set the type for unknown elements to 'unknown'

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/UnknownBlock.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/UnknownBlock.java
@@ -3,12 +3,14 @@ package com.hubspot.slack.client.models.blocks;
 import java.util.Optional;
 
 public class UnknownBlock implements Block {
+  public static final String TYPE = "unknown";
+
   protected UnknownBlock() {
   }
 
   @Override
   public String getType() {
-    return "";
+    return TYPE;
   }
 
   @Override
--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/elements/UnknownBlockElement.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/elements/UnknownBlockElement.java
@@ -1,11 +1,13 @@
 package com.hubspot.slack.client.models.blocks.elements;
 
 public class UnknownBlockElement implements BlockElement {
+  public static final String TYPE = "unknown";
+
   protected UnknownBlockElement() {
   }
 
   @Override
   public String getType() {
-    return "";
+    return TYPE;
   }
 }
