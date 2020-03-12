f5c3b32a2e04cddeb82e19b24094e257a2e21ee9
Whitney Sorenson <wsorenson@hubspot.com>
2020-03-12 15:56:42 -0400

    First pass @ supporting these payloads

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/ViewInteractionPayloadIF.java
@@ -0,0 +1,17 @@
+package com.hubspot.slack.client.models.interaction;
+
+import org.immutables.value.Value.Immutable;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.views.ViewPayloadBase;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+public interface ViewInteractionPayloadIF extends ViewPayloadBase {
+
+  ViewStateValuesPayload getState();
+
+}
