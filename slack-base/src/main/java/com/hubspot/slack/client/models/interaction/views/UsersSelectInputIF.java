f5fb14c3bb6ded6f181e0245616a9cccce685dd1
Todd Pollak <tpollak@hubspot.com>
2020-04-21 12:10:50 -0400

    parse selected user value

--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/UsersSelectInputIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/UsersSelectInputIF.java
@@ -1,11 +1,14 @@
 package com.hubspot.slack.client.models.interaction.views;
 
+import org.immutables.value.Value.Immutable;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
-import org.immutables.value.Value.Immutable;
 
 @Immutable
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
-public interface UsersSelectInputIF extends ViewInput {}
+public interface UsersSelectInputIF extends ViewInput {
+  String getSelectedUser();
+}
