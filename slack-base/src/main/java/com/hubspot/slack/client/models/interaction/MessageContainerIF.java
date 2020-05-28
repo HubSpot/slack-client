18658e62cd94ad3ec53babfb6d9a5a1070182f80
Patrick Lightbody <patrick@reclaimai.com>
2020-05-28 14:52:01 -0700

    support for what appears to be a different format of the payload called out here: https://api.slack.com/legacy/interactive-message-field-guide#options_load_url
    Specifically, it seems that this different payload is what is sent when using BlockKit + external select menus. These classes are useful for deserializing the alternative incoming payload.

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/MessageContainerIF.java
@@ -0,0 +1,18 @@
+package com.hubspot.slack.client.models.interaction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import org.immutables.value.Value.Immutable;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+public interface MessageContainerIF extends Container {
+  String getMessageTs();
+  String getChannelId();
+
+  @JsonProperty("is_ephemeral")
+  boolean isEphemeral();
+}
