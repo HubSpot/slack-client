39b00dee1466c362b327a594579ac68103f4f0e2
Kirtana Suresh <ksuresh@hubspot.com>
2020-07-13 16:17:51 -0400

    Added json property annotation to UserProfileResponse

--- slack-base/src/main/java/com/hubspot/slack/client/models/response/users/UsersProfileResponseIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/users/UsersProfileResponseIF.java
@@ -2,11 +2,17 @@ package com.hubspot.slack.client.models.response.users;
 
 import org.immutables.value.Value.Immutable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.response.SlackResponse;
+import com.hubspot.slack.client.models.users.UserProfile;
 
 @Immutable
 @HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
 public interface UsersProfileResponseIF extends SlackResponse {
-  UsersProfileResponse getUserProfile();
+  @JsonProperty("profile")
+  UserProfile getUserProfile();
 }
