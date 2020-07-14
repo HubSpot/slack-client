6fdedfda7db8e44f758909074a90e5f27f386975
Kirtana Suresh <ksuresh@hubspot.com>
2020-07-14 11:56:19 -0400

    Some refactoring

--- slack-base/src/main/java/com/hubspot/slack/client/models/response/users/UsersProfileResponseIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/users/UsersProfileResponseIF.java
@@ -3,15 +3,12 @@ package com.hubspot.slack.client.models.response.users;
 import org.immutables.value.Value.Immutable;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.response.SlackResponse;
 import com.hubspot.slack.client.models.users.UserProfile;
 
 @Immutable
 @HubSpotStyle
-@JsonNaming(SnakeCaseStrategy.class)
 public interface UsersProfileResponseIF extends SlackResponse {
   @JsonProperty("profile")
   UserProfile getUserProfile();
