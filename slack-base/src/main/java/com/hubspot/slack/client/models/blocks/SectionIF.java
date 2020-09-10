d6c592ca6f25f4d0425b8f2f08ff2dc7849bd07c
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2020-09-10 12:18:35 -0400

    Update to reflect that you can build a section with nothing but fields

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/SectionIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/SectionIF.java
@@ -1,16 +1,17 @@
 package com.hubspot.slack.client.models.blocks;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.immutables.value.Value;
-import org.immutables.value.Value.Immutable;
-
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.blocks.elements.BlockElement;
 import com.hubspot.slack.client.models.blocks.objects.Text;
+import java.util.List;
+import java.util.Optional;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Check;
+import org.immutables.value.Value.Immutable;
 
 @Immutable
 @HubSpotStyle
@@ -25,9 +26,20 @@ public interface SectionIF extends Block {
   }
 
   @Value.Parameter
-  Text getText();
+  Optional<Text> getText();
 
   List<Text> getFields();
 
   Optional<BlockElement> getAccessory();
+
+  @Check
+  default void check() {
+    boolean hasNonEmptyTextField =
+      getText().isPresent() && !Strings.isNullOrEmpty(getText().get().getText());
+    boolean hasFields = !getFields().isEmpty();
+    Preconditions.checkState(
+      hasNonEmptyTextField || hasFields,
+      "Must include text if not providing a list of fields"
+    );
+  }
 }
