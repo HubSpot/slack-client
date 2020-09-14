493eca4cb5a1b02cb0f83b3afb052ee6f1774e57
rxu <rxu@hubspot.com>
2020-09-14 16:55:52 -0400

    Simplify null check for text attr.

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/HeaderIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/HeaderIF.java
@@ -2,14 +2,11 @@ package com.hubspot.slack.client.models.blocks;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
 import com.hubspot.slack.client.models.blocks.objects.Text;
 import com.hubspot.slack.client.models.blocks.objects.TextType;
 import org.immutables.value.Value.Immutable;
 import com.hubspot.immutables.style.HubSpotStyle;
 import org.immutables.value.Value.Check;
-import org.immutables.value.Value.Default;
-import javax.annotation.Nullable;
 import org.immutables.value.Value;
 
 @Immutable
@@ -24,18 +21,11 @@ public interface HeaderIF extends Block {
     return TYPE;
   }
 
-  @Default
-  @Nullable
   @Value.Parameter
-  default Text getText() {
-    return null;
-  }
+  Text getText();
 
   @Check
   default void check() {
-    boolean hasNonEmptyTextField = getText() != null && !Strings.isNullOrEmpty(getText().getText());
-    Preconditions.checkState(hasNonEmptyTextField, "The text of a Header block cannot be empty");
-
     boolean isTextTypeValid = getText().getType() == TextType.PLAIN_TEXT;
     Preconditions.checkState(isTextTypeValid, "The text type of a Header block must be plain_text");
 
