ba50f7e3fe43c4d8a3bc3cb2fc06e66ea7efec42
zmarushchak <zmarushchak@hubspot.com>
2020-10-20 16:42:30 +0300

    Remove unneeded annotation

--- slack-base/src/main/java/com/hubspot/slack/client/models/teams/SlackTeamLiteIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/teams/SlackTeamLiteIF.java
@@ -2,12 +2,10 @@ package com.hubspot.slack.client.models.teams;
 
 import org.immutables.value.Value.Immutable;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hubspot.immutables.style.HubSpotStyle;
 
 @Immutable
 @HubSpotStyle
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public interface SlackTeamLiteIF {
   String getId();
   String getName();
