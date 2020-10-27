6d88fbe0c05daa1603dd7e9498eacede8294e16f
zmarushchak <zmarushchak@hubspot.com>
2020-10-27 17:13:24 +0200

    Add models for group events

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/group/SlackGroupRenameEventIF.java
@@ -0,0 +1,17 @@
+package com.hubspot.slack.client.models.events.group;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.SlackChannel;
+import com.hubspot.slack.client.models.events.SlackEvent;
+import org.immutables.value.Value;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonDeserialize(as = SlackGroupRenameEvent.class)
+@Value.Immutable
+@HubSpotStyle
+public interface SlackGroupRenameEventIF extends SlackEvent {
+  SlackChannel getChannel();
+}
