b599fc831a422d52fb304ed6b9a1f63e7805161d
lhladii <lhladii@hubspot.com>
2021-03-25 11:58:37 +0200

    Resolved PR comments

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/channel/SlackChannelSharedEventIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/channel/SlackChannelSharedEventIF.java
@@ -1,16 +1,13 @@
 package com.hubspot.slack.client.models.events.channel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.events.conversation.SlackConversationEventCore;
 import org.immutables.value.Value;
 
 @Value.Immutable
 @HubSpotStyle
-@JsonNaming(SnakeCaseStrategy.class)
 @JsonDeserialize(as = SlackChannelSharedEvent.class)
 public interface SlackChannelSharedEventIF extends SlackConversationEventCore {
     @JsonProperty("connected_team_id")
