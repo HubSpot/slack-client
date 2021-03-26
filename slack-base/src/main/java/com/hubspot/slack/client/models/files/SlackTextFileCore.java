118b7bf3b7c16f9b727e55a62e7006f298cdd9e3
courtneyadams <81429564+courtneyadams@users.noreply.github.com>
2021-03-26 13:58:29 -0400

    Update SlackTextFileCore.java
    seeing these fields can be null while fetching conversationHistory for a given channel
    error: Cannot build SlackCsvFile, some of required attributes are not set [editLink, preview, previewHighlight, lines, linesMore, previewTruncated]

--- slack-base/src/main/java/com/hubspot/slack/client/models/files/SlackTextFileCore.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/files/SlackTextFileCore.java
@@ -3,11 +3,11 @@ package com.hubspot.slack.client.models.files;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public interface SlackTextFileCore extends SlackFile {
-  String getEditLink();
-  String getPreview();
-  String getPreviewHighlight();
-  String getLines();
-  String getLinesMore();
+  Optional<String> getEditLink();
+  Optional<String> getPreview();
+  Optional<String> getPreviewHighlight();
+  Optional<String> getLines();
+  Optional<String> getLinesMore();
 
   @JsonProperty("preview_is_truncated")
   boolean isPreviewTruncated();
