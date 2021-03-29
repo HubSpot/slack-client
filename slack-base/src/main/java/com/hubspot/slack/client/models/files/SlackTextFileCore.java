e3a7ea1ffd46b1aa9790732223363536d02e20c6
courtneyadams <81429564+courtneyadams@users.noreply.github.com>
2021-03-29 10:06:22 -0400

    Update SlackTextFileCore.java
    Optional class import, my bad!

--- slack-base/src/main/java/com/hubspot/slack/client/models/files/SlackTextFileCore.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/files/SlackTextFileCore.java
@@ -1,6 +1,7 @@
 package com.hubspot.slack.client.models.files;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Optional;
 
 public interface SlackTextFileCore extends SlackFile {
   Optional<String> getEditLink();
