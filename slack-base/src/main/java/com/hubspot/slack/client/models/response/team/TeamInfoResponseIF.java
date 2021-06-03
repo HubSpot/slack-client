6e1bab9d22bf636fa42ee72d2d18c72e11744c36
zmarushchak <zmarushchak@hubspot.com>
2021-06-03 14:06:51 +0300

    Add team name to SlackTeam model

--- slack-base/src/main/java/com/hubspot/slack/client/models/response/team/TeamInfoResponseIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/team/TeamInfoResponseIF.java
@@ -2,6 +2,7 @@ package com.hubspot.slack.client.models.response.team;
 
 import org.immutables.value.Value.Immutable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.response.SlackResponse;
 import com.hubspot.slack.client.models.teams.SlackTeam;
@@ -9,5 +10,6 @@ import com.hubspot.slack.client.models.teams.SlackTeam;
 @Immutable
 @HubSpotStyle
 public interface TeamInfoResponseIF extends SlackResponse {
+  @JsonProperty("team")
   SlackTeam getSlackTeam();
 }
