0345be1b23f56142ccb933029e888abdd47c9f80
aybbr <ayoub.briki@gmail.com>
2021-08-02 15:08:26 +0200

    add mock data

--- slack-base/src/main/java/com/hubspot/slack/client/models/ReactionIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/ReactionIF.java
@@ -3,6 +3,8 @@ package com.hubspot.slack.client.models;
 import java.util.List;
 
 import org.immutables.value.Value.Immutable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
@@ -11,9 +13,10 @@ import com.hubspot.immutables.style.HubSpotStyle;
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
 public interface ReactionIF {
+  @JsonProperty("name")
   String getName();
-
+  @JsonProperty("count")
   Integer getCount();
-
+  @JsonProperty("users")
   List<String> getUser();
 }
