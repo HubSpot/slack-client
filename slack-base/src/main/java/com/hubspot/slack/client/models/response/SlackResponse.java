2ed84e74089f8db2e812abb828c31709b567e068
Stephen Salinas <ssalinas@hubspot.com>
2021-08-25 09:42:39 -0400

    Add JsonProperty annotation for response metadata

--- slack-base/src/main/java/com/hubspot/slack/client/models/response/SlackResponse.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/SlackResponse.java
@@ -2,7 +2,11 @@ package com.hubspot.slack.client.models.response;
 
 import java.util.Optional;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface SlackResponse {
   boolean isOk();
-  Optional<ResponseMetadata> getResponseMetadata();
+
+  @JsonProperty("response_metadata")
+  Optional<com.hubspot.slack.client.models.response.ResponseMetadata> getResponseMetadata();
 }
