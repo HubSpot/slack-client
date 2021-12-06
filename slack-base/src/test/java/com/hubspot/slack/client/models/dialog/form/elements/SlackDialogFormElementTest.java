5ef79b48c1c555861ee4afc64023b3d4ce38e1b1
Oleh Motnyk <omotnyk@hubspot.com>
2021-12-06 13:26:20 +0200

    Cover normalization and some error checks with UTs

---
+++ slack-base/src/test/java/com/hubspot/slack/client/models/dialog/form/elements/SlackDialogFormElementTest.java
@@ -0,0 +1,45 @@
+package com.hubspot.slack.client.models.dialog.form.elements;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.Test;
+
+import com.hubspot.slack.client.testutils.StringGenerator;
+
+public class SlackDialogFormElementTest {
+
+  @Test
+  public void itFailsToBuildDialogFormElementForMissingTooLongName() {
+    String tooLongName = StringGenerator.generateStringWithLength(301);
+    try {
+      SlackFormTextElement.builder().setLabel("ignored").setName(tooLongName).build();
+    } catch (IllegalStateException ise) {
+      assertThat(ise.getMessage()).contains("Name cannot exceed 300 chars, got " + tooLongName);
+      return;
+    }
+
+    fail("Didn't throw exception");
+  }
+
+  @Test
+  public void itNormalizesLongLabelToBuildFormSelectElement() {
+    SlackFormTextElement slackFormTextElement = SlackFormTextElement.builder()
+        .setLabel(StringGenerator.generateStringWithLength(25))
+        .setName("ignored")
+        .build();
+    String expectedLabel = StringGenerator.generateStringWithLengthAndEllipsis(20);
+    assertThat(slackFormTextElement.getLabel()).isEqualTo(expectedLabel);
+  }
+
+  @Test
+  public void itNormalizesLongPlaceholderToBuildFormSelectElement() {
+    SlackFormTextElement slackFormTextElement = SlackFormTextElement.builder()
+        .setLabel("ignored")
+        .setName("ignored")
+        .setPlaceholder(StringGenerator.generateStringWithLength(151))
+        .build();
+    String expectedLabel = StringGenerator.generateStringWithLengthAndEllipsis(146);
+    assertThat(slackFormTextElement.getPlaceholder()).hasValue(expectedLabel);
+  }
+}
---
+++ slack-base/src/test/java/com/hubspot/slack/client/models/dialog/form/elements/SlackFormTextareaElementTest.java
@@ -0,0 +1,36 @@
+package com.hubspot.slack.client.models.dialog.form.elements;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.Test;
+
+import com.hubspot.slack.client.testutils.StringGenerator;
+
+public class SlackFormTextareaElementTest {
+
+  @Test
+  public void itNormalizesLongHintToBuildFormSelectElement() {
+    SlackFormTextareaElement textareaElement = SlackFormTextareaElement.builder()
+        .setLabel("ignored")
+        .setName("ignored")
+        .setHint(StringGenerator.generateStringWithLength(151))
+        .build();
+    String expectedLabel = StringGenerator.generateStringWithLengthAndEllipsis(146);
+    assertThat(textareaElement.getHint()).hasValue(expectedLabel);
+  }
+
+  @Test
+  public void itFailsToBuildDialogFormElementForMissingTooLongValue() {
+    String tooLongValue = StringGenerator.generateStringWithLength(3001);
+    try {
+      SlackFormTextareaElement.builder().setLabel("ignored").setName("ignored").setValue(tooLongValue).build();
+    } catch (IllegalStateException ise) {
+      String errorMessage = String.format("Value cannot exceed 3000 chars, got '%s'", tooLongValue);
+      assertThat(ise.getMessage()).contains(errorMessage);
+      return;
+    }
+
+    fail("Didn't throw exception");
+  }
+}
