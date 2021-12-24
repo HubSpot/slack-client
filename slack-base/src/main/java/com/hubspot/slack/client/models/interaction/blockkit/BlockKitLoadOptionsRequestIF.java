fd08d9aaf1eb46f10e3bfe1ecb951e926ca2c00e
nliutyi <nliutyi@hubspot.com>
2021-12-24 16:23:30 +0200

    Move OptionGroupsResponse to response/blocks package. Use  ViewResponseBase instead of BlockKitView. Remove BlockKitView.

--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/blockkit/BlockKitLoadOptionsRequestIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/blockkit/BlockKitLoadOptionsRequestIF.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.interaction.InteractiveLoadOptionsRequestType;
+import com.hubspot.slack.client.models.response.views.ViewResponseBase;
 import com.hubspot.slack.client.models.teams.SlackTeam;
 import org.immutables.value.Value;
 
@@ -12,7 +13,7 @@ import org.immutables.value.Value;
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public interface BlockKitLoadOptionsRequestIF {
     String getValue();
-    BlockKitView getView();
+    ViewResponseBase getView();
     String getActionId();
     SlackTeam getTeam();
     String getToken();
--- slack-base/src/main/java/com/hubspot/slack/client/models/response/blocks/OptionGroupsResponseIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/blocks/OptionGroupsResponseIF.java
@@ -1,9 +1,10 @@
-package com.hubspot.slack.client.models.blocks.objects;
+package com.hubspot.slack.client.models.response.blocks;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.models.BaseSlackOptionsResponse;
+import com.hubspot.slack.client.models.blocks.objects.OptionGroup;
 import org.immutables.value.Value;
 
 import java.util.List;
