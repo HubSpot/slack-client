77c0374c1b897f06a3c8b1958296ee5a5d8ecfa3
nliutyi <nliutyi@hubspot.com>
2021-12-24 15:44:23 +0200

    Remove duplicating models

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/objects/OptionGroupsResponseIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/objects/OptionGroupsResponseIF.java
@@ -1,14 +1,16 @@
-package com.hubspot.slack.client.models.blocks.options;
+package com.hubspot.slack.client.models.blocks.objects;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.BaseSlackOptionsResponse;
 import org.immutables.value.Value;
+
+import java.util.List;
 
 @Value.Immutable
 @HubSpotStyle
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
-public interface LabelIF {
-    String getType();
-    String getText();
+public interface OptionGroupsResponseIF extends BaseSlackOptionsResponse {
+    List<OptionGroup> getOptionGroups();
 }
