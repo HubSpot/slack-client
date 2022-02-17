afe638924c7c7b1b173a425f0ef89c2e1e0ad10e
zmarushchak <zmarushchak@hubspot.com>
2022-02-17 16:04:38 +0200

    Remove getStringValue method from DatePicker element.

--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewDatePickerIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewDatePickerIF.java
@@ -1,24 +1,17 @@
 package com.hubspot.slack.client.models.interaction.views;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.hubspot.immutables.style.HubSpotStyle;
-import com.hubspot.slack.client.models.blocks.objects.Option;
+import java.time.LocalDate;
+import java.util.Optional;
 
 import org.immutables.value.Value;
 
-import java.time.LocalDate;
-import java.util.Optional;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
 
 @Value.Immutable
 @HubSpotStyle
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public interface ViewDatePickerIF extends ViewInput {
   Optional<LocalDate> getSelectedDate();
-
-  @JsonIgnore
-  default Optional<String> getStringValue() {
-   return Optional.of(getSelectedDate().toString());
-  };
 }
