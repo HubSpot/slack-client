90606bd7b47ec1f34f306844aa0d805c0e25fed9
zmarushchak-hs <66741407+zmarushchak-hs@users.noreply.github.com>
2022-04-05 16:15:49 +0300

    Revert "Add SlackOptionsResponse model for Dialog Options handling (#277)"
    This reverts commit 40fcfe89d7678916767617402791a9311ee69606.

--- slack-base/src/main/java/com/hubspot/slack/client/models/BaseSlackOptionsResponse.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/BaseSlackOptionsResponse.java
@@ -1,14 +1,4 @@
 package com.hubspot.slack.client.models;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.hubspot.slack.client.models.interaction.BlocksLoadOptionsResponse;
-import com.hubspot.slack.client.models.interaction.SlackOptionsResponse;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-@JsonSubTypes({
-    @JsonSubTypes.Type(value = SlackOptionsResponse.class, name = SlackOptionsResponse.TYPE),
-    @JsonSubTypes.Type(value = BlocksLoadOptionsResponse.class, name = BlocksLoadOptionsResponse.TYPE),
-})
 public interface BaseSlackOptionsResponse {
 }
