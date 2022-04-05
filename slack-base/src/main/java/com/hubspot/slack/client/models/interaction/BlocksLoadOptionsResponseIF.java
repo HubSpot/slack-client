2f9b97c1015b9a442f59985148a3e2adbefcaba3
zmarushchak <zmarushchak@hubspot.com>
2022-04-05 10:23:44 +0300

    Add SlackOptionsResponse model

--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/BlocksLoadOptionsResponseIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/BlocksLoadOptionsResponseIF.java
@@ -2,6 +2,7 @@ package com.hubspot.slack.client.models.interaction;
 
 import java.util.List;
 
+import org.immutables.value.Value;
 import org.immutables.value.Value.Check;
 import org.immutables.value.Value.Immutable;
 
@@ -19,6 +20,13 @@ import com.hubspot.slack.client.models.blocks.objects.OptionGroup;
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
 public interface BlocksLoadOptionsResponseIF extends BaseSlackOptionsResponse {
+  String TYPE = "blocks_load_options_response";
+
+  @Value.Derived
+  default String getType() {
+    return TYPE;
+  }
+
   @JsonInclude(Include.NON_EMPTY)
   List<Option> getOptions();
 
