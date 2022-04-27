a6eecaa9b9732f3433bbadaf13c4666d409228af
lhladii <lhladii@hubspot.com>
2022-04-27 17:02:07 +0300

    Remove getting string value for Date/Time pickers.

--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewDatePickerIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewDatePickerIF.java
@@ -1,13 +1,11 @@
 package com.hubspot.slack.client.models.interaction.views;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import org.immutables.value.Value;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Value.Immutable
@@ -15,13 +13,4 @@ import java.util.Optional;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public interface ViewDatePickerIF extends ViewInput {
   Optional<LocalDate> getSelectedDate();
-
-  @JsonIgnore
-  default Optional<String> getStringValue() {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy");
-    if (getSelectedDate().isPresent()) {
-      return Optional.of(getSelectedDate().get().format(formatter));
-    }
-    return Optional.empty();
-  }
 }
--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewTimePickerIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewTimePickerIF.java
@@ -1,13 +1,11 @@
 package com.hubspot.slack.client.models.interaction.views;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import org.immutables.value.Value;
 
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Value.Immutable
@@ -15,13 +13,4 @@ import java.util.Optional;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public interface ViewTimePickerIF extends ViewInput {
   Optional<LocalTime> getSelectedTime();
-
-  @JsonIgnore
-  default Optional<String> getStringValue() {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm");
-    if (getSelectedTime().isPresent()) {
-      return Optional.of(getSelectedTime().get().format(formatter));
-    }
-    return Optional.empty();
-  }
 }
