69875070f02f3733eeb8245db4ce0143a871bec0
Leonid <68000143+leonidhladii@users.noreply.github.com>
2022-04-28 13:59:47 +0300

    Add missing ViewInputType for TimePicker (#284)
    Co-authored-by: lhladii <lhladii@hubspot.com>

--- slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewInputType.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/interaction/views/ViewInputType.java
@@ -7,6 +7,7 @@ import com.hubspot.slack.client.enums.EnumIndex;
 public enum ViewInputType {
   PLAIN_TEXT_INPUT,
   DATEPICKER,
+  TIMEPICKER,
   RADIO_BUTTONS,
   USERS_SELECT,
   EXTERNAL_SELECT,
