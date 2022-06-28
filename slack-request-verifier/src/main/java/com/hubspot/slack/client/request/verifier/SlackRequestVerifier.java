a70ead16b29661c1cc88c8f288ca81dd67e0716d
shurko <shurko@hubspot.com>
2022-06-28 13:05:33 +0300

    added annotation that could be used on the resource method and enable slack request verification logic

---
+++ slack-request-verifier/src/main/java/com/hubspot/slack/client/request/verifier/SlackRequestVerifier.java
@@ -0,0 +1,11 @@
+package com.hubspot.slack.client.request.verifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import com.google.inject.BindingAnnotation;
+
+@BindingAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SlackRequestVerifier {
+}
