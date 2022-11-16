cb86a6517d5ac347f16057ee0a64b9a4ce9ea84c
Oleh Motnyk <olejuuk@gmail.com>
2022-11-16 21:49:36 +0200

    Revert "Add message blocks. (#298)"
    This reverts commit b41dccad6b0c989a86fa51421b60440f59263e16.

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/messages/MessageBlock.java
+++
@@ -1,8 +0,0 @@
-package com.hubspot.slack.client.models.blocks.messages;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.hubspot.slack.client.models.blocks.Block;
-
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
-public interface MessageBlock extends Block {
-}
--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/messages/RichMessageBlock.java
+++
@@ -1,7 +0,0 @@
-package com.hubspot.slack.client.models.blocks.messages;
-
-import java.util.List;
-
-public interface RichMessageBlock extends MessageBlock{
-    List<MessageBlock> getElements();
-}
