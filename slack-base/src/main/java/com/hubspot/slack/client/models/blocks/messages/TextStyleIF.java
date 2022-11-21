ebb2b2d4d62fa48e35419c7583a32a836c1773e8
nliutyi <nliutyi@hubspot.com>
2022-11-21 02:50:52 +0200

    Explicitly set `@JsonProperty()` annotation to evict possible Jackson missmatch which happens with boolean values.

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/messages/TextStyleIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/messages/TextStyleIF.java
@@ -1,5 +1,6 @@
 package com.hubspot.slack.client.models.blocks.messages;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
@@ -11,8 +12,12 @@ import java.util.Optional;
 @HubSpotStyle
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public interface TextStyleIF {
+    @JsonProperty("bold")
     Optional<Boolean> isBold();
+    @JsonProperty("italic")
     Optional<Boolean> isItalic();
+    @JsonProperty("strike")
     Optional<Boolean> isStrike();
+    @JsonProperty("code")
     Optional<Boolean> isCode();
 }
