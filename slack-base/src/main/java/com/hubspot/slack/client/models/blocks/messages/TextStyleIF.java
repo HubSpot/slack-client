ea42f7dd3724291415a2f5b2c38076df9bc9ddc1
nliutyi-hs <96207303+nliutyi-hs@users.noreply.github.com>
2022-11-28 22:44:27 +0200

    Revert "Default to `false` for text styles (#306)" (#307)
    This reverts commit 53221e2a746f29a8a5b546a39ec0681ecc9a02d3.

--- slack-base/src/main/java/com/hubspot/slack/client/models/blocks/messages/TextStyleIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/blocks/messages/TextStyleIF.java
@@ -1,13 +1,10 @@
 package com.hubspot.slack.client.models.blocks.messages;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
 import org.immutables.value.Value;
-import org.immutables.value.Value.Default;
 
 import java.util.Optional;
 
@@ -15,24 +12,12 @@ import java.util.Optional;
 @HubSpotStyle
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public interface TextStyleIF {
-    @Default
     @JsonProperty("bold")
-    default boolean isBold() {
-        return false;
-    }
-    @Default
+    Optional<Boolean> isBold();
     @JsonProperty("italic")
-    default boolean isItalic() {
-        return false;
-    }
-    @Default
+    Optional<Boolean> isItalic();
     @JsonProperty("strike")
-    default boolean isStrikethrough() {
-        return false;
-    }
-    @Default
+    Optional<Boolean> isStrikethrough();
     @JsonProperty("code")
-    default boolean isCode() {
-        return false;
-    }
+    Optional<Boolean> isCode();
 }
