cb15864fb54c92b980f1bb7c18cad32253892dc6
zmarushchak-hs <66741407+zmarushchak-hs@users.noreply.github.com>
2023-01-19 13:31:45 +0200

    Update Team icon URL field (#310)
    * Update Team icon URL
    
    * getImage_88 -> getImage88

--- slack-base/src/main/java/com/hubspot/slack/client/models/teams/SlackTeamIconIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/teams/SlackTeamIconIF.java
@@ -1,17 +1,21 @@
 package com.hubspot.slack.client.models.teams;
 
-import java.util.Optional;
-
-import org.immutables.value.Value.Immutable;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
+import org.immutables.value.Value.Immutable;
+
+import java.util.Optional;
 
 @Immutable
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
 public interface SlackTeamIconIF {
+
+  @JsonProperty("image_88")
+  Optional<String> getImage88();
+
+  @Deprecated
   Optional<String> getImageOriginal();
 }
