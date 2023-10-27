c59b62ea2cea8091daaded8c5e17bc1eed0ba9fb
Ryan Delaney <rydelaney@hubspot.com>
2023-10-27 14:14:00 +0100

    Adding new error types with test files
    These test files are just copied from the existing access_denied error but with the new failure codes. The file_not_found matches the response I've seen on an internal request that failed to deserialise, so I can confirm that one is valid but the file_deleted is a guess.

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/files/SlackFileDeletedFileIF.java
@@ -0,0 +1,15 @@
+package com.hubspot.slack.client.models.files;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import org.immutables.value.Value.Immutable;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+@JsonDeserialize(as = SlackFileDeletedFile.class)
+public interface SlackFileDeletedFileIF extends SlackFileError {
+
+}
---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/files/SlackFileNotFoundFileIF.java
@@ -0,0 +1,16 @@
+package com.hubspot.slack.client.models.files;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import org.immutables.value.Value.Immutable;
+
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+@JsonDeserialize(as = SlackFileNotFoundFile.class)
+public interface SlackFileNotFoundFileIF extends SlackFileError {
+
+}
