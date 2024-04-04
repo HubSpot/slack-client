3f8537bd0b43f9e94407e0fb2df0a34a519b6e2c
ykaliukh <ykaliukh@hubspot.com>
2024-04-04 14:49:52 +0300

    extend AbstractSlackEventMessage from new base

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/AbstractSlackEventMessage.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/AbstractSlackEventMessage.java
@@ -1,32 +1,13 @@
 package com.hubspot.slack.client.models.events;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
-import com.hubspot.slack.client.methods.interceptor.HasUser;
-import java.util.Optional;
 import org.immutables.value.Value.Immutable;
 
 @Immutable
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
 @JsonDeserialize(as = SlackEventMessage.class)
-public abstract class AbstractSlackEventMessage
-  extends SlackEventMessageBase
-  implements HasUser {
-
-  @JsonProperty("channel")
-  public abstract String getChannelId();
-
-  public abstract Optional<String> getThreadTs();
-
-  public abstract Optional<String> getBotId();
-
-  @Override
-  @JsonProperty("user")
-  public abstract String getUserId();
-
-  public abstract String getText();
-}
+public abstract class AbstractSlackEventMessage extends SlackEventMessageExtendedBase {}
