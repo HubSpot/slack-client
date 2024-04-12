f66837de7cf64a603b6df8d0a984d61f74506f48
Jared Stehler <jstehler@hubspot.com>
2024-04-12 10:18:48 -0400

    move deprecated guice module to slack-java-client-guice

--- slack-java-client-guice/src/main/java/com/hubspot/slack/client/guice/SlackClientModule.java
+++ slack-java-client-guice/src/main/java/com/hubspot/slack/client/guice/SlackClientModule.java
@@ -1,23 +1,32 @@
 package com.hubspot.slack.client.guice;
 
 import com.google.inject.AbstractModule;
+import com.hubspot.slack.client.SlackClientFactory;
+import com.hubspot.slack.client.SlackWebClient;
+import com.hubspot.slack.client.http.NioHttpClient;
+import com.hubspot.slack.client.http.NioHttpClientFactory;
 import com.hubspot.slack.client.request.verifier.SlackRequestVerifierModule;
 
 public class SlackClientModule extends AbstractModule {
 
-  @Override
-  protected void configure() {
-    install(new com.hubspot.slack.client.SlackClientModule());
-    install(new SlackRequestVerifierModule());
-  }
+    @Override
+    protected void configure() {
+        bind(NioHttpClientFactory.class).toInstance(NioHttpClientFactory.defaultFactory());
+        bind(NioHttpClient.Factory.class).toInstance(NioHttpClientFactory.defaultFactory());
 
-  @Override
-  public boolean equals(Object o) {
-    return o != null && getClass().equals(o.getClass());
-  }
+        bind(SlackClientFactory.class).toInstance(SlackClientFactory.defaultFactory());
+        bind(SlackWebClient.Factory.class).toInstance(SlackClientFactory.defaultFactory());
 
-  @Override
-  public int hashCode() {
-    return getClass().hashCode();
-  }
+        install(new SlackRequestVerifierModule());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o != null && getClass().equals(o.getClass());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- slack-java-client/src/test/java/com/hubspot/slack/client/interceptors/http/SlackClientModuleTest.java
+++ slack-java-client/src/test/java/com/hubspot/slack/client/interceptors/http/SlackClientModuleTest.java
@@ -3,23 +3,22 @@ package com.hubspot.slack.client.interceptors.http;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Stage;
-import com.hubspot.slack.client.SlackClientModule;
 import org.junit.Test;
 
 public class SlackClientModuleTest {
 
-  @Test
-  public void itGuices() {
-    Guice.createInjector(Stage.TOOL, new StrictGuiceModule());
-  }
-
-  private static class StrictGuiceModule extends AbstractModule {
-
-    @Override
-    protected void configure() {
-      binder().requireAtInjectOnConstructors();
-
-      install(new SlackClientModule());
+    @Test
+    public void itGuices() {
+        Guice.createInjector(Stage.TOOL, new StrictGuiceModule());
     }
-  }
+
+    private static class StrictGuiceModule extends AbstractModule {
+
+        @Override
+        protected void configure() {
+            binder().requireAtInjectOnConstructors();
+
+            install(new SlackClientModule());
+        }
+    }
 }
