aac9d46f290e1021f32217ced4c77fdc392f80dc
ykaliukh <ykaliukh@hubspot.com>
2024-05-10 12:21:43 +0300

    do not include empty thread ts

--- slack-base/src/main/java/com/hubspot/slack/client/methods/params/files/CompleteUploadExternalParamsIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/files/CompleteUploadExternalParamsIF.java
@@ -1,5 +1,6 @@
 package com.hubspot.slack.client.methods.params.files;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
@@ -10,6 +11,7 @@ import org.immutables.value.Value.Immutable;
 @Immutable
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public interface CompleteUploadExternalParamsIF {
   List<FileInfo> getFiles();
   Optional<String> getChannelId();
