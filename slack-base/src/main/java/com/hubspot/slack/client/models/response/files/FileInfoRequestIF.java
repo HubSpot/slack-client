0600d8aa51e63c8b7f3de64443b13968afb4165c
ykaliukh <ykaliukh@hubspot.com>
2024-09-30 15:23:35 +0300

    add FileInfoRequestIF

---
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/files/FileInfoRequestIF.java
@@ -0,0 +1,16 @@
+package com.hubspot.slack.client.models.response.files;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.models.response.SlackResponse;
+import org.immutables.value.Value.Immutable;
+
+@Immutable
+@HubSpotStyle
+@JsonNaming(SnakeCaseStrategy.class)
+public interface FileInfoRequestIF extends SlackResponse {
+  @JsonProperty("file")
+  String fileId();
+}
