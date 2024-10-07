b07c6c1739e0668be29e1e6ba141eeaf95f603c6
ykaliukh <ykaliukh@hubspot.com>
2024-10-07 17:00:51 +0300

    delete wrong extension

--- slack-base/src/main/java/com/hubspot/slack/client/models/response/files/FileInfoRequestIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/response/files/FileInfoRequestIF.java
@@ -4,13 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
-import com.hubspot.slack.client.models.response.SlackResponse;
 import org.immutables.value.Value.Immutable;
 
 @Immutable
 @HubSpotStyle
 @JsonNaming(SnakeCaseStrategy.class)
-public interface FileInfoRequestIF extends SlackResponse {
+public interface FileInfoRequestIF {
   @JsonProperty("file")
   String fileId();
 }
