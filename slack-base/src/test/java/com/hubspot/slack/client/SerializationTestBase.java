f4652e0b6341a988101b0eff220f48c2e314d479
Jared Stehler <jstehler@hubspot.com>
2025-05-28 17:19:09 -0400

    revert changes to test base

--- slack-base/src/test/java/com/hubspot/slack/client/SerializationTestBase.java
+++ slack-base/src/test/java/com/hubspot/slack/client/SerializationTestBase.java
@@ -2,12 +2,10 @@ package com.hubspot.slack.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hubspot.slack.client.jackson.ObjectMapperUtils;
 import com.hubspot.slack.client.models.JsonLoader;
 import java.io.IOException;
-import java.util.Map;
 
 public class SerializationTestBase {
 
@@ -18,16 +16,7 @@ public class SerializationTestBase {
     String rawJson = JsonLoader.loadJsonFromFile(filename);
     T jsonObject = OBJECT_MAPPER.readValue(rawJson, jsonClass);
     String generatedJson = OBJECT_MAPPER.writeValueAsString(jsonObject);
-
-    Map<String, Object> actual = OBJECT_MAPPER.readValue(
-      rawJson,
-      new TypeReference<Map<String, Object>>() {}
-    );
-    Map<String, Object> expected = OBJECT_MAPPER.readValue(
-      generatedJson,
-      new TypeReference<Map<String, Object>>() {}
-    );
-
-    assertThat(actual).isEqualTo(expected);
+    assertThat(OBJECT_MAPPER.readTree(rawJson))
+      .isEqualTo(OBJECT_MAPPER.readTree(generatedJson));
   }
 }
