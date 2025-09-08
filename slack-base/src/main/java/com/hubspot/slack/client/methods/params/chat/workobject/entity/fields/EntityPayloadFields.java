49921bfb8e0d41452bc9d9d2d2db3db0aad1af90
Viktor Pelepiak <vpelepiak@hubspot.com>
2025-09-08 11:46:53 +0300

    Create the base entity fields model and it's implementation for the Item entity

---
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/chat/workobject/entity/fields/EntityPayloadFields.java
@@ -0,0 +1,3 @@
+package com.hubspot.slack.client.methods.params.chat.workobject.entity.fields;
+
+public interface EntityPayloadFields {}
---
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/chat/workobject/entity/fields/ItemFieldsIF.java
@@ -0,0 +1,8 @@
+package com.hubspot.slack.client.methods.params.chat.workobject.entity.fields;
+
+import com.hubspot.immutables.style.HubSpotStyle;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@HubSpotStyle
+public interface ItemFieldsIF extends EntityPayloadFields {}
