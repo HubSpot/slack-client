03e02b0c12f449ca606da19e9c24d81bb926f549
Viktor Pelepiak <vpelepiak@hubspot.com>
2025-09-16 11:33:54 +0300

    Update models to omit empty fields during serialization

--- slack-base/src/main/java/com/hubspot/slack/client/methods/params/chat/workobject/entity/EntityPayloadAttributesIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/chat/workobject/entity/EntityPayloadAttributesIF.java
@@ -1,5 +1,8 @@
 package com.hubspot.slack.client.methods.params.chat.workobject.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.hubspot.immutables.style.HubSpotStyle;
 import com.hubspot.slack.client.methods.params.chat.workobject.serializers.DisplayTypeSerializer;
@@ -8,10 +11,13 @@ import org.immutables.value.Value;
 
 @Value.Immutable
 @HubSpotStyle
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public interface EntityPayloadAttributesIF {
   EntityPayloadAttributeTitle getTitle();
   Optional<String> getDisplayId();
 
+  @JsonInclude(JsonInclude.Include.NON_ABSENT)
   @JsonSerialize(using = DisplayTypeSerializer.class)
   Optional<DisplayType> getDisplayType();
 
--- slack-base/src/main/java/com/hubspot/slack/client/methods/params/chat/workobject/entity/fields/ItemFieldsIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/methods/params/chat/workobject/entity/fields/ItemFieldsIF.java
@@ -1,8 +1,10 @@
 package com.hubspot.slack.client.methods.params.chat.workobject.entity.fields;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hubspot.immutables.style.HubSpotStyle;
 import org.immutables.value.Value;
 
 @Value.Immutable
 @HubSpotStyle
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public interface ItemFieldsIF extends EntityPayloadFields {}
