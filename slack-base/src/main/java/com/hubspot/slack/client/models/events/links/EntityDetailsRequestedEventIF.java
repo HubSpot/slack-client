42a488a41b36b1ff18baeee18fb572e01dcbb5ba
Viktor Pelepiak <vpelepiak@hubspot.com>
2025-10-04 19:42:17 +0300

    Add missing properties for the EntityDetailsRequestedEvent model

--- slack-base/src/main/java/com/hubspot/slack/client/models/events/links/EntityDetailsRequestedEventIF.java
+++ slack-base/src/main/java/com/hubspot/slack/client/models/events/links/EntityDetailsRequestedEventIF.java
@@ -1,8 +1,34 @@
 package com.hubspot.slack.client.models.events.links;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.hubspot.immutables.style.HubSpotStyle;
+import com.hubspot.slack.client.methods.params.chat.workobject.ExternalRef;
+import com.hubspot.slack.client.models.events.SlackEvent;
+import java.util.Optional;
 import org.immutables.value.Value;
 
 @HubSpotStyle
 @Value.Immutable
-public interface EntityDetailsRequestedEventIF {}
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonDeserialize(as = EntityDetailsRequestedEvent.class)
+public interface EntityDetailsRequestedEventIF extends SlackEvent {
+  @JsonProperty("user")
+  String getUserId();
+
+  @JsonProperty("channel")
+  Optional<String> getChannelId();
+
+  ExternalRef getExternalRef();
+  String getEntityUrl();
+  Link getLink();
+  String getAppUnfurlUrl();
+  String getEventTs();
+  String getTriggerId();
+  String getUserLocale();
+
+  Optional<String> getMessageTs();
+  Optional<String> getThreadTs();
+}
