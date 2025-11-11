2a1912859a464f4a9e5f8ff72ab555178892de84
jstehler <jstehler@hubspot.com>
2025-11-11 14:55:52 -0500

    upgrade horizon to 0.5.0, netty from 3 to 4

--- slack-java-client-examples/src/main/java/com/hubspot/slack/server/example/SlackMessageHandler.java
+++ slack-java-client-examples/src/main/java/com/hubspot/slack/server/example/SlackMessageHandler.java
@@ -1,12 +1,5 @@
 package com.hubspot.slack.server.example;
 
-import com.hubspot.horizon.shaded.org.jboss.netty.buffer.ChannelBuffer;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.Channel;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.ChannelHandlerContext;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.ExceptionEvent;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.MessageEvent;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.SimpleChannelHandler;
-import com.hubspot.horizon.shaded.org.jboss.netty.handler.codec.http.HttpRequest;
 import com.hubspot.slack.client.SlackClient;
 import com.hubspot.slack.client.examples.BasicRuntimeConfig;
 import com.hubspot.slack.client.jackson.ObjectMapperUtils;
@@ -20,14 +13,19 @@ import com.hubspot.slack.client.models.interaction.BlockElementAction;
 import com.hubspot.slack.client.models.interaction.SlackInteractiveCallback;
 import com.hubspot.slack.client.models.response.views.ModalViewCommandResponse;
 import com.hubspot.slack.client.models.views.ModalViewPayload;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpObject;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SlackMessageHandler extends SimpleChannelHandler {
+public class SlackMessageHandler extends SimpleChannelInboundHandler<HttpObject> {
 
   private static final Logger LOG = LoggerFactory.getLogger(SlackMessageHandler.class);
 
@@ -38,36 +36,42 @@ public class SlackMessageHandler extends SimpleChannelHandler {
   }
 
   @Override
-  public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
-    HttpRequest request = (HttpRequest) e.getMessage();
-    ChannelBuffer content = request.getContent();
-    try {
-      String jsonContent = URLDecoder
-        .decode(content.toString(StandardCharsets.UTF_8), "utf-8")
-        .substring(8);
-      SlackInteractiveCallback callback = ObjectMapperUtils
-        .mapper()
-        .readValue(jsonContent, SlackInteractiveCallback.class);
-      LOG.info("Received raw JSON: {}", jsonContent);
-      LOG.info("Deserialized Callback: {}", callback);
-      if (callback instanceof BlockActions) {
-        sendResponse((BlockActions) callback);
+  protected void channelRead0(
+    ChannelHandlerContext channelHandlerContext,
+    HttpObject msg
+  ) throws Exception {
+    if (msg instanceof HttpContent) {
+      HttpContent httpContent = (HttpContent) msg;
+      ByteBuf content = httpContent.content();
+
+      try {
+        String jsonContent = URLDecoder
+          .decode(content.toString(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
+          .substring(8);
+        SlackInteractiveCallback callback = ObjectMapperUtils
+          .mapper()
+          .readValue(jsonContent, SlackInteractiveCallback.class);
+        LOG.info("Received raw JSON: {}", jsonContent);
+        LOG.info("Deserialized Callback: {}", callback);
+        if (callback instanceof BlockActions) {
+          sendResponse((BlockActions) callback);
+        }
+      } catch (IOException ex) {
+        LOG.error("Could not decode message", ex);
       }
-    } catch (IOException ex) {
-      LOG.error("Could not decode message", ex);
     }
   }
 
   @Override
-  public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
-    LOG.error("Got an error", e.getCause());
-    Channel ch = e.getChannel();
-    ch.close();
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+    throws Exception {
+    LOG.error("Got an error", cause.getCause());
+    ctx.close();
   }
 
   private void sendResponse(BlockActions blockActions) {
     for (BlockElementAction action : blockActions.getElementActions()) {
-      LOG.info("You interaced with: {}", action);
+      LOG.info("You interacted with: {}", action);
     }
 
     ModalViewCommandResponse response = slackClient
@@ -76,7 +80,7 @@ public class SlackMessageHandler extends SimpleChannelHandler {
           blockActions.getTriggerId(),
           ModalViewPayload.of(
             Text.of(TextType.PLAIN_TEXT, "Hi " + blockActions.getUser().getUsername()),
-            Arrays.asList(
+            Collections.singletonList(
               Section
                 .of(Text.of(TextType.MARKDOWN, "Thanks for clicking on _something_!"))
                 .withAccessory(DatePicker.of("my-date-picker"))
--- slack-java-client-examples/src/main/java/com/hubspot/slack/server/example/SlackServer.java
+++ slack-java-client-examples/src/main/java/com/hubspot/slack/server/example/SlackServer.java
@@ -1,35 +1,51 @@
 package com.hubspot.slack.server.example;
 
-import com.hubspot.horizon.shaded.org.jboss.netty.bootstrap.ServerBootstrap;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.ChannelFactory;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.Channels;
-import com.hubspot.horizon.shaded.org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
-import com.hubspot.horizon.shaded.org.jboss.netty.handler.codec.http.HttpChunkAggregator;
-import com.hubspot.horizon.shaded.org.jboss.netty.handler.codec.http.HttpServerCodec;
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
 
 public class SlackServer {
 
-  public static void main(String[] args) {
-    ChannelFactory factory = new NioServerSocketChannelFactory(
-      Executors.newCachedThreadPool(),
-      Executors.newCachedThreadPool()
-    );
+  public static void main(String[] args) throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b
+        .group(bossGroup, workerGroup)
+        .channel(NioServerSocketChannel.class)
+        .option(ChannelOption.SO_BACKLOG, 100)
+        .localAddress(8080)
+        .childOption(ChannelOption.TCP_NODELAY, true)
+        .childOption(ChannelOption.SO_KEEPALIVE, true)
+        .childHandler(
+          new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) throws Exception {
+              ch.pipeline().addLast(new HttpRequestDecoder(), new SlackMessageHandler());
+            }
+          }
+        );
 
-    ServerBootstrap bootstrap = new ServerBootstrap(factory);
+      // Start the server.
+      ChannelFuture f = b.bind().sync();
 
-    bootstrap.setPipelineFactory(() ->
-      Channels.pipeline(
-        new HttpServerCodec(),
-        new HttpChunkAggregator(2 << 10),
-        new SlackMessageHandler()
-      )
-    );
+      // Wait until the server socket is closed.
+      f.channel().closeFuture().sync();
+    } finally {
+      // Shut down all event loops to terminate all threads.
+      bossGroup.shutdownGracefully();
+      workerGroup.shutdownGracefully();
 
-    bootstrap.setOption("child.tcpNoDelay", true);
-    bootstrap.setOption("child.keepAlive", true);
-
-    bootstrap.bind(new InetSocketAddress(8080));
+      // Wait until all threads are terminated.
+      bossGroup.terminationFuture().sync();
+      workerGroup.terminationFuture().sync();
+    }
   }
 }
